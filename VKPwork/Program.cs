f5f6da75f75aba383895df079dfa624889e58769
Tupikova Anastasiya <tupikova.a.00@yandex.ru>
2024-05-12 14:34:12 +0700

    Исключить состояние, когда все 4 цепи отключаются

--- VKPwork/Program.cs
+++ VKPwork/Program.cs
@@ -28,10 +28,10 @@ namespace VKPwork
 		/// исходных списков.</exception>
 		public static List<double> CompareLists(List<double> X, List<double> Y)
 		{
-			//if (X.Count != Y.Count)
-			//{
-			//	throw new ArgumentException("Списки X и Y должны иметь одинаковую длину.");
-			//}
+			if (X.Count != Y.Count)
+			{
+				throw new ArgumentException("Списки X и Y должны иметь одинаковую длину.");
+			}
 
 			List<double> results = new List<double>();
 
@@ -145,7 +145,7 @@ namespace VKPwork
 			List<double> randSostTaksimoMamakan2 = new List<double>();
 
 			// Генерация случайного числа ГЕНЕРАЦИИ в цикле с условием
-			while (randValueGen.Count < 10)
+			while (randValueGen.Count < 105409)
 			{
 				double q = rand.NextDouble();
 
@@ -178,7 +178,7 @@ namespace VKPwork
 			}
 
 			// Генерация случайного числа НАГРУЗКИ в цикле с условием
-			while (randValueLoad.Count < 10)
+			while (randValueLoad.Count < 105409)
 			{
 				double q = rand.NextDouble();
 				if (q >= 0 && q < v4)
@@ -214,8 +214,8 @@ namespace VKPwork
 			double s1 = 1;
 			double s2 = 0;
 			// Генерация случайного состояния ЦЕПИ линии
-			while (randSostPeledSyxLog1.Count < 10 && randSostPeledSyxLog2.Count < 10 
-				&& randSostTaksimoMamakan1.Count < 10 && randSostTaksimoMamakan2.Count < 10)
+			while (randSostPeledSyxLog1.Count < 105409 && randSostPeledSyxLog2.Count < 105409 
+				&& randSostTaksimoMamakan1.Count < 105409 && randSostTaksimoMamakan2.Count < 105409)
 			{
 				double q1 = rand.NextDouble();
 				double q2 = rand.NextDouble();
@@ -307,7 +307,7 @@ namespace VKPwork
 			double numberYR = 0;
 
 			// Цикл расчета перетоков в RastrWin3
-			for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 105409; i++)
 			{
 				// Присвоение нового числа мощности генерации
 				var setSelAgr = "Num=" + 2;
@@ -395,7 +395,7 @@ namespace VKPwork
 			worksheet3.Name = "Состояние линий";
 
 			// Запись значений в файл Excel
-			for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 105409; i++)
 			{
 				// Получаем диапазон ячеек начиная с ячейки A1
 				Range range1 = worksheet1.Range["A1"];
