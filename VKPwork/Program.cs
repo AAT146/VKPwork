8a14759f0caf41a8e4f76f4269bf2adf3b946497
Tupikova Anastasiya <tupikova.a.00@yandex.ru>
2024-05-20 17:02:53 +0700

    добавлено сохранение в файл Ecxel

--- VKPwork/Program.cs
+++ VKPwork/Program.cs
@@ -18,7 +18,8 @@ namespace VKPwork
 		static void Main(string[] args)
 		{
 			// Укажите путь к вашему файлу Excel
-			string filePath = "path_to_your_excel_file.xlsx";
+			string filePath = "C:\\Users\\Анастасия\\Desktop\\2в10.xlsx";
+			string outputPath = "C:\\Users\\Анастасия\\Desktop\\результат2в10.xlsx";
 
 			// Проверка существования файла
 			if (!File.Exists(filePath))
@@ -29,14 +30,19 @@ namespace VKPwork
 
 			// Открытие и чтение файла Excel
 			FileInfo fileInfo = new FileInfo(filePath);
+			FileInfo outputFileInfo = new FileInfo(outputPath);
+
 			using (ExcelPackage package = new ExcelPackage(fileInfo))
 			{
 				// Получение первого рабочего листа
 				ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
 				int rowCount = worksheet.Dimension.Rows;
 
+				// Добавление заголовка для нового столбца
+				//worksheet.Cells[1, 2].Value = "Decimal Value";
+
 				// Перебор строк в столбце A
-				for (int row = 1; row <= rowCount; row++)
+				for (int row = 1; row <= rowCount; row++) // Начинаем с 2, чтобы пропустить заголовок
 				{
 					string binaryString = worksheet.Cells[row, 1].Text;
 
@@ -46,19 +52,24 @@ namespace VKPwork
 						{
 							// Конвертация из двоичной в десятичную систему
 							int decimalValue = Convert.ToInt32(binaryString, 2);
-							Console.WriteLine($"Строка {row}: {binaryString} в десятичной системе = {decimalValue}");
+							worksheet.Cells[row, 2].Value = decimalValue;
 						}
 						catch (FormatException)
 						{
-							Console.WriteLine($"Строка {row}: Неверный формат числа \"{binaryString}\".");
+							worksheet.Cells[row, 2].Value = "Invalid Format";
 						}
 					}
 					else
 					{
-						Console.WriteLine($"Строка {row}: Пустая ячейка.");
+						worksheet.Cells[row, 2].Value = "Empty Cell";
 					}
 				}
+
+				// Сохранение результата в новый файл
+				package.SaveAs(outputFileInfo);
 			}
+
+			Console.WriteLine($"Результаты сохранены в {outputPath}");
 		}
 	}
 }
